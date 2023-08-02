99c932393ca38287990a58e96cfb1df23e8f71ed
berkayaskin <berkayaskiin@gmail.com>
2023-08-02 17:58:26 +0300

    style(fag-section.tsx): update faq-section design
    use accordions from shadcn/ui instead of plain text

--- src/app/pricing/_components/faq-section.tsx
+++ src/app/pricing/_components/faq-section.tsx
@@ -1,4 +1,16 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion'
 import { faq } from '@/data/pricing-page'
+{
+  /* <div className="px-4 py-6 md:w-1/2" key={index}>
+            <div className="h4 mb-2">{item.question}</div>
+            <p className="leading-normal">{item.answer}</p>
+          </div> */
+}
 
 const Faq = () => {
   return (
@@ -6,10 +18,21 @@ const Faq = () => {
       <h2 className="h2 mb-4 leading-tight">FAQ</h2>
       <div className="-mx-4 flex flex-wrap">
         {faq.map((item, index) => (
-          <div className="px-4 py-6 md:w-1/2" key={index}>
-            <div className="h4 mb-2">{item.question}</div>
-            <p className="leading-normal">{item.answer}</p>
-          </div>
+          <Accordion
+            type="single"
+            key={index}
+            collapsible
+            className="w-full px-4 py-6"
+          >
+            <AccordionItem value="index">
+              <AccordionTrigger className="h4 mb-2 font-bold">
+                {item.question}
+              </AccordionTrigger>
+              <AccordionContent className="text-lg leading-normal">
+                {item.answer}
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
         ))}
       </div>
     </div>
